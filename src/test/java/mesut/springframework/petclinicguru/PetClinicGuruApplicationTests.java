9c852ffbf3f470adfa257f673ce53a96c00d6362
MessaoudAzidane <fm_azidane@esi.dz>
2019-08-01 13:59:28 +0100

    Initial Project commit

---
+++ src/test/java/mesut/springframework/petclinicguru/PetClinicGuruApplicationTests.java
@@ -0,0 +1,16 @@
+package mesut.springframework.petclinicguru;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetClinicGuruApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
